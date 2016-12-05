e206201b433c3c089096a767728e05e9390d2c0e
osmirmariano <osmirmariano@uft.edu.br>
2016-12-05 09:45:45 -0200

    Inciando implementação do trabalho 5 de compiladores

--- main.cpp
+++ main.cpp
@@ -3,6 +3,50 @@
 using namespace std;
 
 int main(int argc, char const *argv[]){
-	
+	int op;
+	AnalisadorSintatico *sintatico =  new AnalisadorSintatico();
+
+	do{
+        cout << "---------------------------------------------------" << endl;
+        cout << "\t MENU DE OPÇÃO" << endl;
+        cout << "---------------------------------------------------" << endl;
+        cout << " 1 -- CARREGAR ARQUIVO" << endl;
+        cout << " 2 -- ENTRADA CÓDIGO FONTE" << endl;
+        cout << " 0 -- SAIR" << endl;
+        cout << "---------------------------------------------------" << endl;
+        cout << " OPÇÃO: ";
+        cin >> op;
+        cout << "---------------------------------------------------" << endl;
+
+        switch(op){
+            case 1:
+                cout << "---------------------------------------------------" << endl;
+                cout << "\t ARQUIVO CARREGADO" << endl;
+                cout << "---------------------------------------------------" << endl;
+                break;
+
+            case 2:
+                cout << "---------------------------------------------------" << endl;
+                cout << "\t ENTRADA CÓDIGO FONTE" << endl;
+                cout << "---------------------------------------------------" << endl;
+                cout << " ENTRADA: ";
+                cin.ignore();
+                getline (cin, entrada);
+                cout << "---------------------------------------------------" << endl;
+                
+                cout << "---------------------------------------------------" << endl;
+                break;
+
+            case 0:
+                cout << "\tAPLICAÇÃO ENCERRADA COM SUCESSO!" << endl << endl;
+                break;
+
+            default:
+                cout << "\tOPÇÃO INVÁLIDA, POR FAVOR ESCOLHA UMA VÁLIDA" << endl;
+        }
+        cout << endl << "  PARA VOLTAR AO MENU PRESSIONE ENTER" << endl;
+        cin.ignore().get();//Comando para Pausa no terminal
+        cout << "\e[H\e[2J";//Comando para limpar a tela
+    }while(op != 0);
 	return 0;
 }
