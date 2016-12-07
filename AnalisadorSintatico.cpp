398c5c9ef812272c1d26d2717df2a9da24e42e7e
osmirmariano <osmirmariano@uft.edu.br>
2016-12-07 00:16:35 -0200

    Novas Atualizações

--- AnalisadorSintatico.cpp
+++ AnalisadorSintatico.cpp
@@ -7,7 +7,7 @@ using namespace std;
 class AnalisadorSintatico{
       public:
             //Os que seria E' substitui por A e os que seria T' substitui por B
-            string terminais[5] = {"E", "A", "T", "B", "F"};
+            string terminais[5] = {"E", "E'", "T", "T'", "F"};
 	      string variaveis[6] = {"id", "+", "*", "(", ")", "$"};
 	      string tabela[5][6];
 
@@ -19,30 +19,30 @@ class AnalisadorSintatico{
 
 		void definindoTabela(){
 			int x, y;
-			tabela[0][0] = {"TA"};
+			tabela[0][0] = {"TE'"};
                   tabela[0][1] = {" "};
                   tabela[0][2] = {" "};
-                  tabela[0][3] = {"TA"};
+                  tabela[0][3] = {"TE'"};
                   tabela[0][4] = {" "};
                   tabela[0][5] = {" "};
 
                   tabela[1][0] = {" "};
-                  tabela[1][1] = {"+TA"};
+                  tabela[1][1] = {"+TE'"};
                   tabela[1][2] = {" "};
                   tabela[1][3] = {" "};
                   tabela[1][4] = {"&"};
                   tabela[1][5] = {"&"};
 
-                  tabela[2][0] = {"FB"};
+                  tabela[2][0] = {"FT'"};
                   tabela[2][1] = {" "};
                   tabela[2][2] = {" "};
-                  tabela[2][3] = {"FB"};
+                  tabela[2][3] = {"FT'"};
                   tabela[2][4] = {" "};
                   tabela[2][5] = {" "};
 
                   tabela[3][0] = {" "};
                   tabela[3][1] = {"&"};
-                  tabela[3][2] = {"*FB"};
+                  tabela[3][2] = {"*FT'"};
                   tabela[3][3] = {" "};
                   tabela[3][4] = {"&"};
                   tabela[3][5] = {"&"};
@@ -97,6 +97,7 @@ class AnalisadorSintatico{
                         for(x = 0; x < 5; x++){
                               for(y = 0; y < 6; y++){
                                     if(pilha.top() == terminais[x] && variaveis[y] == novoVetor[z]){
+                                          cout << ""
                                           if(tabela[x][y] != " "){
                                                 pilha.pop();
                                                 quebra = tabela[x][y];
